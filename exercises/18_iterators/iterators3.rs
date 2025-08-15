b0459c3a995230bf6c196f2f8b56f866acda4043
刘景宇 <2537738252@qq.com>
2025-08-15 11:31:35 +0800

    fix error reported in learning-tools xtask eval

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -25,7 +25,7 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
 // Desired output: `Ok([1, 11, 1426, 3])`
 fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results = numbers.into_iter().map(|n| divide(*n as i64, 27));
     division_results.collect()
 }
 
@@ -33,7 +33,7 @@ fn result_with_list() -> Result<Vec<i64>, DivisionError> {
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
 fn list_of_results() -> Vec<Result<i64, DivisionError>> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results = numbers.into_iter().map(|n| divide(*n as i64, 27));
     division_results.collect()
 }
 
