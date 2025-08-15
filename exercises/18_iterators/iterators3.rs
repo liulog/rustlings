52a801845f4d99dc970afd221294721685e8aa65
刘景宇 <2537738252@qq.com>
2025-08-15 11:15:41 +0800

    fix bugs

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -25,16 +25,16 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
 // Desired output: `Ok([1, 11, 1426, 3])`
 fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    numbers.into_iter().map(|n| divide(n, 27))
-        .collect::<Result<Vec<i64>, DivisionError>>()
+    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    division_results.collect()
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
 fn list_of_results() -> Vec<Result<i64, DivisionError>> {
     let numbers = [27, 297, 38502, 81];
-    numbers.into_iter().map(|n| divide(n, 27))
-        .collect::<Vec<Result<i64, DivisionError>>>()
+    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    division_results.collect()
 }
 
 fn main() {
